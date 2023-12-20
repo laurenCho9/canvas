dc0108d4fbf77e5e851d8fe160ee840d2b49a2e5
laurenCho9 <lauren.choux@gmail.com>
2023-12-21 02:53:32 +0900

    직선 그리기

--- app.js
+++ app.js
@@ -5,15 +5,27 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(210 - 40, 200 - 30, 15, 100);
-ctx.fillRect(350 - 40, 200 - 30, 15, 100);
-ctx.fillRect(260 - 40, 200 - 30, 60, 200);
+const colors = [
+  "#ff3838",
+  "#ffb8b8",
+  "#c56cf0",
+  "#ff9f1a",
+  "#fff200",
+  "#32ff7e",
+  "#7efff5",
+];
 
-ctx.arc(250, 100, 50, 0, 2 * Math.PI);
-ctx.fill();
+ctx.lineWidth = 2;
 
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(260 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.fill();
+function onClick(event) {
+  //   console.log(event);
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
+
+// canvas.addEventListener("click", onClick);
+canvas.addEventListener("mousemove", onClick);
