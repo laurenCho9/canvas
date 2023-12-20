a24fb210c7b9b3ea83570843c773aa9f98715b27
laurenCho9 <lauren.choux@gmail.com>
2023-12-21 02:30:15 +0900

    사람 그리기

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200); // shortCut(지름길) function으로 fill과 rect를 호출
-ctx.fillRect(400, 200, 50, 200);
-ctx.lineWidth = 2; // context의 스타일이 먼저 와야 적용된다.
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
+ctx.fillRect(210 - 40, 200 - 30, 15, 100);
+ctx.fillRect(350 - 40, 200 - 30, 15, 100);
+ctx.fillRect(260 - 40, 200 - 30, 60, 200);
+
+ctx.arc(250, 100, 50, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(260 + 10, 80, 8, Math.PI, 2 * Math.PI);
+ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI);
 ctx.fill();
