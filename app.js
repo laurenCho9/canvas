c3c53e43227c8d23cc53eef1300084581a6e0aec
laurenCho9 <lauren.choux@gmail.com>
2023-12-21 04:03:02 +0900

    드래그로 그리기(ex 전자서명란)

--- app.js
+++ app.js
@@ -4,28 +4,25 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-
-const colors = [
-  "#ff3838",
-  "#ffb8b8",
-  "#c56cf0",
-  "#ff9f1a",
-  "#fff200",
-  "#32ff7e",
-  "#7efff5",
-];
-
 ctx.lineWidth = 2;
+let isPainting = false;
 
-function onClick(event) {
-  //   console.log(event);
-  ctx.beginPath();
-  ctx.moveTo(0, 0);
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
+function startPainting(event) {
+  isPainting = true;
+}
+function cancelPainting(event) {
+  isPainting = false;
 }
 
-// canvas.addEventListener("click", onClick);
-canvas.addEventListener("mousemove", onClick);
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
